63cddd7c01b84b0948de87d92f0ab671ba8ca9dc
theslim21 <tesbolajiyusuf@gmail.com>
2023-04-16 12:44:42 +0100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "BJ"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
+*/
 
   required_providers {
     aws = {
